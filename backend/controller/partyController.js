5d10ba322a8066a966377fd7f569942d4a377320
Virgílio César <enjels@outlook.com>
2024-04-26 11:01:13 -0300

    adicionado metodo update

--- backend/controller/partyController.js
+++ backend/controller/partyController.js
@@ -76,6 +76,36 @@ const partyController = {
     const deletedParty = await PartyModel.findByIdAndDelete(id);
 
     return res.status(200).json({deletedParty, msg: "Festa excluída com sucesso." });
-},
+  },
+  update: async (req, res) => {
+
+    try {
+const id = req.params.id;
+      
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento é insuficiente." });
+        return;
+      }
+      
+      const updateParty = await PartyModel.findByIdAndUpdate(id, party);
+
+      if (!updateParty) {
+        return res.status(404).json({ msg: "Festa não encontrada." });
+      }
+
+      res.status(200).json({party, msg: "Festa atualizada com sucesso." });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
-module.exports = partyController;
+module.exports = partyController
