d482ecf589323a3efe5f964d1572efbeebbd6277
Virgílio César <enjels@outlook.com>
2024-04-26 10:30:30 -0300

    update de metodos

--- backend/controller/partyController.js
+++ backend/controller/partyController.js
@@ -23,18 +23,59 @@ const partyController = {
         image: req.body.image,
         services: req.body.services,
       };
-        if (party.services && checkPartyBudget(party.budget, party.services)) {
+        if (party.services && !checkPartyBudget(party.budget, party.services)) {
             res.status(406).json({ msg: "O seu orçamento é insuficiente." })
             return;
         }
-        
+
         const response = await PartyModel.create(party)
 
         res.status(201).json({ response, msg: "Festa criada com sucesso" })
-        
+
     } catch (error) {
       console.log(error);
     }
   },
+  getAll: async (req, res) => {
+    try {
+      const parties = await PartyModel.find()
+      
+      res.json(parties)
+    } catch (error) {
+
+      console.log(error);
+    }
+
+  },
+  get: async (req, res) => {
+    try {
+
+      const id = await req.params.id
+
+      const party = await PartyModel.findById(id)
+
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada." })
+        return;
+      }
+
+      res.json(party)
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    const id = req.params.id;
+
+    const party = await PartyModel.findById(id);
+
+    if (!party) {
+        return res.status(404).json({ msg: "Festa não encontrada." });
+    }
+
+    const deletedParty = await PartyModel.findByIdAndDelete(id);
+
+    return res.status(200).json({deletedParty, msg: "Festa excluída com sucesso." });
+},
 };
 module.exports = partyController;
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 
-const partyController = require("../controller/partyController")
-router.route("/parties").post((req, res) => {partyController.create(req, res);});
+const partyController = require("../controller/partyController");
+router.route("/parties").post((req, res) => partyController.create(req, res));
+
+router.route("/parties").get((req, res) => partyController.getAll(req, res));
+
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
+
+router.route("/parties/:id").delete((req, res) => partyController.delete(req, res));
+
 //* Funções
 
 module.exports = router;
